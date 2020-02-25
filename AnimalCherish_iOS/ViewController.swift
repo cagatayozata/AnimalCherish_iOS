432f4bdbcd1d48dea5dea63842b0f87af9b8ae32
Cagatay Ozata <cagatayozata@Cagatays-MacBook-Pro.local>
2020-02-25 14:30:56 +0300

    iOS Application created

---
+++ AnimalCherish_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimalCherish_iOS
+//
+//  Created by Cagatay Ozata on 25.02.2020.
+//  Copyright © 2020 CTIS_Team1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
