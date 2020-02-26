65e354b0eb3ea546ba5ceb4f14ffc288083e7377
Metehan <metehan.kara95@gmail.com>
2020-02-27 01:19:17 +0300

    HomePage - Login - Register Pages First Layout Design

--- AnimalCherish_iOS/ViewControllers/Auth/RegisterViewController.swift
+++ AnimalCherish_iOS/ViewControllers/Auth/RegisterViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+           // Do any additional setup after loading the view.
     }
-    
+
 
     /*
     // MARK: - Navigation
--- AnimalCherish_iOS/ViewControllers/HomeViewController.swift
+++ AnimalCherish_iOS/ViewControllers/HomeViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+ 
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      
+        
+       
         // Do any additional setup after loading the view.
     }
     
