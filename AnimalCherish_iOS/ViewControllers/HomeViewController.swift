02a8bf8d94c822054aeaaaf1e81efcea237f5e7b
Çağatay Özata <cagatayozata@me.com>
2020-02-28 21:27:55 +0300

    Revert "HomePage - Login - Register Pages First Layout Design"

--- AnimalCherish_iOS/ViewControllers/HomeViewController.swift
+++ AnimalCherish_iOS/ViewControllers/HomeViewController.swift
@@ -9,15 +9,10 @@
 import UIKit
 
 class HomeViewController: UIViewController {
- 
-    @IBOutlet weak var loginButton: UIButton!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        
-       
+
         // Do any additional setup after loading the view.
     }
     
