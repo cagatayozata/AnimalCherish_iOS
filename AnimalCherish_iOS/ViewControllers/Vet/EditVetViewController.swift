b5fa55e12ac52402f3191fa695f3ef9d2e90d73f
Cagatay Ozata <cagatayozata@Cagatays-MacBook-Pro.local>
2020-03-10 21:12:57 +0300

    tum screenler icin detail ve edit controlleri eklendi

---
+++ AnimalCherish_iOS/ViewControllers/Vet/EditVetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditVetViewController.swift
+//  AnimalCherish_iOS
+//
+//  Created by Cagatay Ozata on 10.03.2020.
+//  Copyright © 2020 CTIS_Team1. All rights reserved.
+//
+
+import UIKit
+
+class EditVetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
