f19716794b934820149c4592bfd9d06a30e3d575
Cagatay Ozata <cagatayozata@Cagatays-MacBook-Pro.local>
2020-03-10 18:39:40 +0300

    UITableView ve GET request listeleme sayfasi altyapisi kuruldu, http isteklere izin verildi, Configuration dosyasi olsuturuldu

---
+++ AnimalCherish_iOS/Other/Configuration.swift
@@ -0,0 +1,15 @@
+//
+//  GlobalVariables.swift
+//  AnimalCherish_iOS
+//
+//  Created by Cagatay Ozata on 10.03.2020.
+//  Copyright © 2020 CTIS_Team1. All rights reserved.
+//
+
+import Foundation
+
+struct Configuration {
+
+    static let apiUrl = "http://localhost:8081"
+
+}
