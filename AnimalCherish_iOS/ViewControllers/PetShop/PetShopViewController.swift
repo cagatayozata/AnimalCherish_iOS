18e74412e49fd5141b5a2203cad2be414db5cbc1
Cagatay Ozata <cagatayozata@Cagatays-MacBook-Pro.local>
2020-03-10 18:50:01 +0300

    Pet Shop GET istegi tamamlandi, UITableView eklendi baglandi

--- AnimalCherish_iOS/ViewControllers/PetShop/PetShopViewController.swift
+++ AnimalCherish_iOS/ViewControllers/PetShop/PetShopViewController.swift
@@ -7,24 +7,105 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class PetShopViewController: UIViewController {
+class PetShopViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // MARK: IBOutlet
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: Variables
+    let apiUrl = Configuration.apiUrl + "/animal/getall"
+    
+    var petShopIdArr = [String]()
+    var petShopNameArr = [String]()
+    var petShopEmailArr = [String]()
+    var petShopPhoneArr = [String]()
+        
+    // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
+        getPetShopList()
+        
     }
     
+    // MARK: Data Preparation and GET request
+    func getPetShopList() {
+        
+        AF.request(apiUrl, method: .get).responseJSON { (myresponse) in
+            
+            // check result is success or failure
+            switch myresponse.result {
+            case .success:
+                
+                // removeAll
+                self.petShopIdArr.removeAll()
+                self.petShopNameArr.removeAll()
+                self.petShopEmailArr.removeAll()
+                self.petShopPhoneArr.removeAll()
+                
+                // GET data
+                let myresult = try? JSON(data: myresponse.data!)
+                let resultArray = myresult!
+                
+                //
+                for item in resultArray.arrayValue {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                    let id = item["id"].stringValue
+                    let name = item["name"].stringValue
+                    let email = item["email"].stringValue
+                    let phone = item["phone"].stringValue
+                    
+                    self.petShopIdArr.append(id)
+                    self.petShopNameArr.append(name)
+                    self.petShopEmailArr.append(email)
+                    self.petShopPhoneArr.append(phone)
+                    
+                }
+                
+                // reload table data
+                self.tableView.reloadData()
+                
+                break
+            case .failure:
+                self.showAlert(for: "Bir hata oluştu. Hayvan Listesi Getiriemedi!")
+                print(myresponse.error!)
+                break
+            }
+    
+        }
     }
-    */
+    
+    // MARK: Alert
+    func showAlert(for alert: String) {
+        let alertController = UIAlertController(title: nil, message: alert, preferredStyle: UIAlertController.Style.alert)
+        let alertAction = UIAlertAction(title: "Tamam", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: UITableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petShopIdArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: "animalcell")
+         if cell == nil {
+             cell = UITableViewCell(style: .subtitle, reuseIdentifier: "animalcell")
+         }
+         
+        cell?.textLabel?.text = self.petShopNameArr[indexPath.row]
+        cell?.detailTextLabel?.text = (self.petShopEmailArr[indexPath.row] ) + ", " + (self.petShopPhoneArr[indexPath.row] )
+        
+         return cell!
+        
+    }
 
 }
