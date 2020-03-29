040c7b381bf64a1cb393dfeae90d036a8a3619ec
cagatayozata <cagatayozata@me.com>
2020-03-29 23:02:25 +0300

    Login ve Register ekranlarinin tasarimlari ve fonskiyonlari tamamlandi

--- AnimalCherish_iOS/ViewControllers/Auth/LoginViewController.swift
+++ AnimalCherish_iOS/ViewControllers/Auth/LoginViewController.swift
@@ -10,21 +10,75 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var box: UIView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var forgetPasswordButton: UIButton!
+    @IBOutlet weak var createUserButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Keyboard
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        // Box
+        box.layer.cornerRadius = 15
+        box.layer.borderWidth = 1
+        
+        // Username
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: "Kullanıcı Adı",
+        attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        usernameTextField.setLeftImage(imageName: "person")
+        
+        // Password
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Parola",
+        attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        passwordTextField.setLeftImage(imageName: "lock")
+        
+        // Login Button
+        loginButton.layer.cornerRadius = 5
+        loginButton.layer.borderWidth = 1
+        
+        // Forget Password
+        let attributedString = NSAttributedString(string: NSLocalizedString("Şifremi Unuttum", comment: ""), attributes:[
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15.0),
+            NSAttributedString.Key.foregroundColor : UIColor.black,
+            NSAttributedString.Key.underlineStyle:1.0
+        ])
+        forgetPasswordButton.setAttributedTitle(attributedString, for: .normal)
+        
+        // Create an account
+        let attributedString1 = NSAttributedString(string: NSLocalizedString("Hesap Oluştur", comment: ""), attributes:[
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15.0),
+            NSAttributedString.Key.foregroundColor : UIColor.black,
+            NSAttributedString.Key.underlineStyle:1.0
+        ])
+        createUserButton.setAttributedTitle(attributedString1, for: .normal)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+        // when clicking the UIView, keyboard will be removed
+        self.view.endEditing(true)
+    
     }
-    */
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
 
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
 }
--- AnimalCherish_iOS/ViewControllers/Auth/RegisterViewController.swift
+++ AnimalCherish_iOS/ViewControllers/Auth/RegisterViewController.swift
@@ -10,21 +10,73 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var box: UIView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var alreadyRegisterdButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Keyboard
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        // Box
+        box.layer.cornerRadius = 15
+        box.layer.borderWidth = 1
+        
+        // Username
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: "Kullanıcı Adı",
+        attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        usernameTextField.setLeftImage(imageName: "person")
+        
+        // E-mail
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Email",
+        attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        emailTextField.setLeftImage(imageName: "envelope")
+        
+        // Password
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Parola",
+        attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        passwordTextField.setLeftImage(imageName: "lock")
+        
+        // Login Button
+        registerButton.layer.cornerRadius = 5
+        registerButton.layer.borderWidth = 1
+        
+        // Already Registered Button
+        let attributedString = NSAttributedString(string: NSLocalizedString("Zaten üyeyim, giriş yap", comment: ""), attributes:[
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15.0),
+            NSAttributedString.Key.foregroundColor : UIColor.black,
+            NSAttributedString.Key.underlineStyle:1.0
+        ])
+        alreadyRegisterdButton.setAttributedTitle(attributedString, for: .normal)
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+        // when clicking the UIView, keyboard will be removed
+        self.view.endEditing(true)
+    
     }
-    */
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
 
 }
+
