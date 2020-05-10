3b838ff39a1fcbc26b547d3da90b0811c750c485
cagatayozata <cagatayozata@me.com>
2020-05-11 00:16:18 +0300

    test yazildi getler icin

--- AnimalCherish_iOSTests/AnimalCherish_iOSTests.swift
+++ AnimalCherish_iOSTests/AnimalCherish_iOSTests.swift
@@ -6,9 +6,14 @@
 //  Copyright © 2020 CTIS_Team1. All rights reserved.
 //
 
+import Alamofire
 import XCTest
 
-class AnimalCherish_iOSTests: XCTestCase {
+struct Configuration {
+    static let apiUrl = "http://138.68.67.165"
+}
+
+class AnimalUnit: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,15 +22,98 @@ class AnimalCherish_iOSTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func getTest() throws {
+        let tempApiUrl = Configuration.apiUrl + "/api/v1/animal/getall"
+
+        AF.request(tempApiUrl, method: .get).responseJSON { myresponse in
+
+            XCTAssertNil(myresponse.error, "Whoops, error \(myresponse.error!.localizedDescription)")
+            XCTAssertNotNil(myresponse, "No response")
+            XCTAssertEqual(myresponse.response?.statusCode ?? 0, 200, "Status code not 200")
+        }
+    }
+}
+
+class PetShopUnit: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func getTest() throws {
+        let tempApiUrl = Configuration.apiUrl + "/api/v1/petshop/getall"
+
+        AF.request(tempApiUrl, method: .get).responseJSON { myresponse in
+
+            XCTAssertNil(myresponse.error, "Whoops, error \(myresponse.error!.localizedDescription)")
+            XCTAssertNotNil(myresponse, "No response")
+            XCTAssertEqual(myresponse.response?.statusCode ?? 0, 200, "Status code not 200")
+        }
+    }
+}
+
+class ShelterUnit: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func getTest() throws {
+        let tempApiUrl = Configuration.apiUrl + "/api/v1/shelter/getall"
+
+        AF.request(tempApiUrl, method: .get).responseJSON { myresponse in
+
+            XCTAssertNil(myresponse.error, "Whoops, error \(myresponse.error!.localizedDescription)")
+            XCTAssertNotNil(myresponse, "No response")
+            XCTAssertEqual(myresponse.response?.statusCode ?? 0, 200, "Status code not 200")
+        }
+    }
+}
+
+class VetUnit: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func getTest() throws {
+        let tempApiUrl = Configuration.apiUrl + "/api/v1/vet/getall"
+
+        AF.request(tempApiUrl, method: .get).responseJSON { myresponse in
+
+            XCTAssertNil(myresponse.error, "Whoops, error \(myresponse.error!.localizedDescription)")
+            XCTAssertNotNil(myresponse, "No response")
+            XCTAssertEqual(myresponse.response?.statusCode ?? 0, 200, "Status code not 200")
+        }
+    }
+}
+
+class ZooUnit: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func getTest() throws {
+        let tempApiUrl = Configuration.apiUrl + "/api/v1/zoo/getall"
+
+        AF.request(tempApiUrl, method: .get).responseJSON { myresponse in
+
+            XCTAssertNil(myresponse.error, "Whoops, error \(myresponse.error!.localizedDescription)")
+            XCTAssertNotNil(myresponse, "No response")
+            XCTAssertEqual(myresponse.response?.statusCode ?? 0, 200, "Status code not 200")
         }
     }
 }
